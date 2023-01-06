70fa669f4445f6f91935444243073408f9f09f77
Florian Melzig <florian@melzig.com>
2023-01-06 18:09:58 +0100

    add train5G

---
+++ proj/src/tumtrain/TumTrainApp5G.h
@@ -0,0 +1,89 @@
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+// 
+// You should have received a copy of the GNU Lesser General Public License
+// along with this program.  If not, see http://www.gnu.org/licenses/.
+// 
+
+#ifndef SRC_TUMTRAIN_TUMTRAINAPP5G_H_
+#define SRC_TUMTRAIN_TUMTRAINAPP5G_H_
+
+#include <vector>
+
+#include "inet/applications/base/ApplicationBase.h"
+#include "inet/common/clock/ClockUserModuleMixin.h"
+#include "inet/transportlayer/contract/udp/UdpSocket.h"
+
+using namespace inet;
+
+namespace inet {
+    extern template class ClockUserModuleMixin<ApplicationBase>;
+}
+
+/**
+ * TODO - Generated class
+ */
+class TumTrainApp5G : public ClockUserModuleMixin<ApplicationBase>, public UdpSocket::ICallback
+{
+  protected:
+    enum SelfMsgKinds { START = 1, SEND, STOP };
+
+    // parameters
+    int trainId = -1;
+    int trackId = 0;
+
+    std::vector<L3Address> destAddresses;
+    std::vector<std::string> destAddressStr;
+    int localPort = -1, destPort = -1;
+    clocktime_t startTime;
+    clocktime_t stopTime;
+    bool dontFragment = false;
+    const char *packetName = nullptr;
+
+    // state
+    UdpSocket socket;
+    ClockEvent *selfMsg = nullptr;
+
+    // statistics
+    int numSent = 0;
+    int numReceived = 0;
+
+  protected:
+    virtual int numInitStages() const override { return NUM_INIT_STAGES; }
+    virtual void initialize(int stage) override;
+    virtual void handleMessageWhenUp(cMessage *msg) override;
+    virtual void finish() override;
+    virtual void refreshDisplay() const override;
+
+    // chooses random destination address
+    virtual L3Address chooseDestAddr();
+    virtual void sendPacket();
+    virtual void processPacket(Packet *msg);
+    virtual void setSocketOptions();
+
+    virtual void processStart();
+    virtual void processSend();
+    virtual void processStop();
+
+    virtual void handleStartOperation(LifecycleOperation *operation) override;
+    virtual void handleStopOperation(LifecycleOperation *operation) override;
+    virtual void handleCrashOperation(LifecycleOperation *operation) override;
+
+    virtual void socketDataArrived(UdpSocket *socket, Packet *packet) override;
+    virtual void socketErrorArrived(UdpSocket *socket, Indication *indication) override;
+    virtual void socketClosed(UdpSocket *socket) override;
+
+  public:
+    TumTrainApp5G() {};
+    ~TumTrainApp5G();
+};
+
+#endif /* SRC_TUMTRAIN_TUMTRAINAPP5G_H_ */
